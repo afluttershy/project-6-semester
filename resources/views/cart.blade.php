1baa2d64978fd2b66aa14941aaf0e25cd36e2f84
afluttershy <russulaaa@gmail.com>
2020-06-04 03:50:05 +0500

    add cart and menu-item pages

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,1 +1,83 @@
 @extends('layout')
+
+@section('content')
+    <div class="cart-page">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('/') }}">Главная</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Корзина</li>
+            </ol>
+        </nav>
+
+        
+        <h1>Корзина</h1>
+        <div class="cart-divs">
+            <div class="cart-one">
+                <img src="../img/square-images/pizza1.png">
+                <div>
+                    <h3>Пицца «Сингапур» на тонком тесте 30 см</h3>
+                    <p>130 г</p>
+                </div>
+                <p class="price">999 &#8381;</p>
+                <a href="#"><img class="icon-delete" src="img/icon-delete.png"></a>
+            </div> 
+            <div class="cart-one">
+                <img src="../img/square-images/pizza1.png">
+                <div>
+                    <h3>Пицца «Сингапур» на тонком тесте 30 см</h3>
+                    <p>130 г</p>
+                </div>
+                <p class="price">999 &#8381;</p>
+                <a href="#"><img class="icon-delete" src="img/icon-delete.png"></a>
+            </div> 
+        </div>
+
+
+
+        <form class="order-address-form">
+            <h2>Укажите адрес доставки</h2>
+            <div class="form-div adress">
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="street">Улица</label>
+                        <input requried type="text" class="form-control" id="street">
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="house">Дом</label>   
+                        <input requried type="text" class="form-control" id="house">
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label for="flat">Квартира</label>
+                        <input requried type="text" class="form-control" id="flat">
+                    </div>
+                </div>
+            </div>
+            <h2>Ваши контактные данные</h2>
+            <div class="form-div contacts">
+                <div class="form-group">
+                    <div>
+                        <label for="name">Имя</label>
+                        <input requried class="form-control" type="text" id="name" name="name">
+                    </div>
+                    <div>
+                        <label for="tel">Телефон</label>
+                        <input requried class="form-control" type="number" id="tel" name="tel">
+                    </div>
+                </div>
+            </div>
+            <h2>Оплата при получении</h2>
+            <div class="form-div payment">
+                <div class="form-group">
+                    <label><input type="checkbox" name="option1" value="1">Наличными</label>
+                    <label><input type="checkbox" name="option2" value="2">Банковской картой</label>
+                </div>
+            </div>
+            <div class="bottom">
+                <a href="#">Страница для печати деталей заказа</a>
+                <button type="submit" class="btn-auth">Оформить заказ</button>
+            </div>
+        </form>
+
+
+    </div>
+@endsection
--- resources/views/menu-item.blade.php
+++ resources/views/menu-item.blade.php
@@ -11,15 +11,39 @@
     </nav>
     <div class="menu-item">
         <div class="menu-item-top">
-            <img src="../img/square-images/pizza1.png">
+            <img class="item-picture" src="../img/square-images/pizza1.png">
             <div>
-                <h2>Название</h2>
-                <p>Описание</p>
-                <h3>Цена: 999 &#8381;<h3>
+                <h2>Пицца «Сингапур» на тонком тесте 30 см</h2>
+                <p>Нежная курица, острый халапеньо и соус том ям на тонком тесте практически без корочки — авторская пицца в азиатском стиле.</p>
+                <h3>Цена: 999 &#8381;</h3>
+                <a href="#" class="btn-main">В корзину <img class="icon" src="../img/icon-cart.png"></a>
             </div>
-        <div>
-        <div class="menu-item-bottom">
         </div>
+        <div class="menu-item-bottom">
+            <h3>Состав:</h3>
+            <p>Куриное филе бедра марин., помидоры, красный лук, перец халапеньо, сырный соус, соус том ям, сыр моцарелла, тесто дрожжевое, мука в/с из твердых сортов пшеницы.</p>
+            <div class="energy">
+                <h3>Энергетическая ценность (на 100 г):</h3>
+                <div>
+                    <h4>Белки</h4>
+                    <p>9 г</p>
+                </div>
+                <div>
+                    <h4>Жиры</h4>
+                    <p>11 г</p>
+                </div>
+                <div>
+                    <h4>Углеводы</h4>
+                    <p>17 г</p>
+                </div>
+                <div>
+                    <h4>Энерг. ценность</h4>
+                    <p>200 кКал</p>
+                </div>
+            </div>
+
+        </div>
+        <img class="menu-item-person" src="img/person-goodinfo.png">
     </div>
 </div>
 @endsection
